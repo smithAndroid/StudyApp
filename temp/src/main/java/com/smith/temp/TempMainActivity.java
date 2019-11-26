fa14655cf83eedd2720e29a3bec52d7aa978b2a4
smith <smith@wang.com>
2019-11-26 20:30:22 +0800

    在master分支上为TempMainActivity文件做一次临时修改，修改临时字符串tmp="world"

--- temp/src/main/java/com/smith/temp/TempMainActivity.java
+++ temp/src/main/java/com/smith/temp/TempMainActivity.java
@@ -92,7 +92,7 @@ public class TempMainActivity extends BaseActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        String tmp = null;
+        String tmp = "world";
         resetDagger();
     }
 
