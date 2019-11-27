7c1d15be4ae617e338d0c418b1a36817bef1fb8a
smith <smith@wang.com>
2019-11-27 21:37:13 +0800

    private String revert1 = "git revert111111";

--- temp/src/main/java/com/smith/temp/git/GitTest.java
+++ temp/src/main/java/com/smith/temp/git/GitTest.java
@@ -3,5 +3,6 @@ package com.smith.temp.git;
 public class GitTest {
     private String reset = "git reset";
     private String revert = "git revert";
+    private String revert1 = "git revert111111";
 
 }
